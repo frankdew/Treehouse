55ad7cf71d3bd58cbdc008a8dc47f36b0a7c5657
frankdew <frankdew23@gmail.com>
2016-04-11 17:03:00 +0200

    listings changes made

--- db/schema.rb
+++ db/schema.rb
@@ -18,11 +18,11 @@ ActiveRecord::Schema.define(version: 20160411124859) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
-    t.text     "descripion"
+    t.text     "description"
     t.string   "location"
     t.string   "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
