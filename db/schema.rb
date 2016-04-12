8620547cf00ffd0e52da116b0f41a2f07ea5b7b8
frankdew <frankdew23@gmail.com>
2016-04-12 15:28:59 +0200

    Add images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411124859) do
+ActiveRecord::Schema.define(version: 20160412130632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160411124859) do
     t.string   "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "photo"
   end
 
 end
