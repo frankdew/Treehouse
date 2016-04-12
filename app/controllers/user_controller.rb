0b9aaa8bbc12620af175929c90a9363183d879cf
“habdallah78git config --global user.email “hisham.abdallah@outlook.com <Hisham@Hishams-MacBook-Pro.local>
2016-04-12 15:55:42 +0200

    added attributes to user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,18 @@
 class UserController < ApplicationController
+  # GET /users/new
+    def new
+      @user = User.new
+    end
+
+    # POST /users
+    # POST /users.json
+    def create
+      @user = User.new(user_params)
+
+      if @user.save
+        redirect_to root_path, notice: "Hi, #{@user.name}!"
+      else
+        render :new
+      end
+    end
 end
