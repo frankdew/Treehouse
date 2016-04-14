989bbba88ab010bfa2d66bda9baf9c108848c3f1
HashtAggie <a.el.arras@gmail.com>
2016-04-14 14:43:10 +0200

    Did some styling on the listing page and added buttons and fotos

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 Listing.create(name:"Treehouse Ashley", description: "a treehouse for the elite", location: "England", price:600, photo: "bamboo-treehouse-27.jpg")
 Listing.create(name:"Treehouse Frank", description: "a treehouse for the the common man", location: "The Netherlands", price:100, photo: "bg_lodge2.jpg")
 Listing.create(name:"Treehouse Aggie", description: "a treehouse for the frustrated man", location: "Morocco", price:200, photo:"Designer-Treehouses.jpg")
-Listing.create(name:"Treehouse Hicham", description: "a treehouse for the romantic soul", location: "Egypt", price:300, photo: "images.jpeg")
+Listing.create(name:"Treehouse Hisham", description: "a treehouse for the romantic soul", location: "Egypt", price:300, photo: "images.jpeg")
 Listing.create(name:"Treehouse Brian", description: "a treehouse for the thinkers among us", location: "Spain", price:250, photo: "Lake-District-Tree-House-3-1024x819.jpg")
 Listing.create(name:"Treehouse Nora", description: "a treehouse for moon lovers", location: "China", price:400, photo: "temple-sh-4.jpg")
 Listing.create(name:"Treehouse Marian", description: "a treehouse for sun lovers", location: "Germany", price:450, photo: "Tree-House-Chunnambar-Boat-House.jpg")
