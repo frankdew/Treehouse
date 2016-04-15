92b52d2034acb639a1f540c95a40291c281dd9bb
HashtAggie <a.el.arras@gmail.com>
2016-04-15 13:26:32 +0200

    Fixed it!

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,8 +2,8 @@ class ListingsController < ApplicationController
   def index
     @listings = Listing.all
   end
-  
-  before_show :authenticate_user!
+
+  # before_show :authenticate_user!
   def show
       @listing = Listing.find( params[:id] )
    end
