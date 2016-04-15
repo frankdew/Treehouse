d88d6c731254f8b984be69e84ac28e98c3340436
“habdallah78git config --global user.email “hisham.abdallah@outlook.com <Hisham@Hishams-MacBook-Pro.local>
2016-04-15 12:04:53 +0200

    fixing deviseuser

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,7 +2,8 @@ class ListingsController < ApplicationController
   def index
     @listings = Listing.all
   end
-
+  
+  before_show :authenticate_user!
   def show
       @listing = Listing.find( params[:id] )
    end
