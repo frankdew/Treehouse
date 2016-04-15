7deaaa2a148322375fd261931f3ce799ffc9356c
HashtAggie <a.el.arras@gmail.com>
2016-04-15 15:44:33 +0200

    done for today

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  get 'about' => 'listings#about'
-  get 'contact' => 'listings#contact'
-   get 'listings' => 'listings#index'
-   root 'listings#index'
-   get 'listings/:id' => 'listings#show', as: :listing
-  get 'search' => 'listings#search'
+root 'listings#index'
+    get 'about' => 'listings#about'
+    get 'contact' => 'listings#contact'
+    get 'listings' => 'listings#index'
+    get 'listings/:id' => 'listings#show', as: :listing
+    get 'search' => 'listings#index'
 end
